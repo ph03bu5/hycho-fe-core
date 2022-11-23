1d891471bb5d8febeb6f478a6761f2008aa808f8
Hang Young CHO <unclecho@anchors-biz.com>
2022-11-23 12:44:24 +0900

    slightly updated in definitions

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ import AnchorsInput from './lib/components/anchors-input/anchors-input';
 import AnchorsDataTable from './lib/components/data-table/anchors-data-table';
 import API, {ApiResponse} from './lib/helpers/api-helper';
 import useHookForm from './lib/helpers/form-helper';
+
+export { AnchorsInput, AnchorsDataTable, API, ApiResponse, useHookForm };
