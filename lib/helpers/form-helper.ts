09f3b31db2c6b60af836d08a74a123dc39a49fe1
Hang Young CHO <unclecho@anchors-biz.com>
2023-01-17 17:31:22 +0900

    added setFocus in useHookForm

--- lib/helpers/form-helper.ts
+++ lib/helpers/form-helper.ts
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 const useHookForm = (defaultValues: any = {}) => {
   const form = useForm({defaultValues});
   const formHandle = { register: form.register, control: form.control, errors: form.formState.errors };
-  const handles = { handleSubmit: form.handleSubmit, reset: form.reset, getValues: form.getValues, setValue: form.setValue, formState: form.formState };
+  const handles = { handleSubmit: form.handleSubmit, reset: form.reset, getValues: form.getValues, setValue: form.setValue,  setFocus: form.setFocus, formState: form.formState };
   return {...handles, formHandle};
 };
 
